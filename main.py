731c2a9a5219aafe7c1582f87a27e2921bed4437
arsel_wat <120033385+arselwat@users.noreply.github.com>
2023-01-03 16:32:22 +0100

    je viens d'ajouter la classe cercle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from math import pi
 from abc import ABCMeta, abstractmethod 
 
 class forme_geometrique(metaclass=ABCMeta):
@@ -19,3 +19,15 @@ class rectangle(forme_geometrique):
     
     def perimetre(self):
         return (self.hauteur+self.larg)*2
+    
+# 'jai importé le module math pour utiliser "pi" 
+
+class cercle(forme_geometrique):
+    def __init__(self, rayon):
+        self.rayon=rayon
+    def surface(self):
+        return pi*self.rayon**2
+    def perimetre(self):
+        return 2*pi*self.rayon
+    
+# ajoute la classe triangle 
