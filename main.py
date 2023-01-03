6a55df7b2b097f7ea8d8d816137db1b03376afeb
Nelson-Ngombo <120034661+Nelson-Ngombo@users.noreply.github.com>
2023-01-03 16:22:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,4 +8,14 @@ class forme_geometrique(metaclass=ABCMeta):
     def surface(self):
         pass  
     def perimetre(self):
-        pass    
+        pass
+class rectangle(forme_geometrique):
+    
+    def __init__(self,hauteur,larg):
+        self.hauteur=hauteur
+        self.larg=larg
+    def surface(self):
+        return self.hauteur*self.larg
+    
+    def perimetre(self):
+        return (self.hauteur+self.larg)*2
