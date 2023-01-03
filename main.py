2b0541bf132a32c3e4afc5cf8f2092f0100f19cf
arsel_wat <120033385+arselwat@users.noreply.github.com>
2023-01-03 16:18:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from abc import ABCMeta, abstractmethod 
+
+class forme_geometrique(metaclass=ABCMeta):
+    
+    @abstractmethod
+    
+    def surface(self):
+        pass  
+    def perimetre(self):
+        pass    
