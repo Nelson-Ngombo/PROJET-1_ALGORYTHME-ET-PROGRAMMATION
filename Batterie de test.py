7a915405e3d7256a734c35aa3073dac349bb3e1c
arsel_wat <120033385+arselwat@users.noreply.github.com>
2023-01-04 19:46:23 +0100

    ici, j'ai ajouté le calcule du temps d'exécution
    ici, j'ai ajouté le batterie de test en ajoutant le calcule du temps d'exécution

--- Batterie de test.py
+++ Batterie de test.py
@@ -1,56 +1,60 @@
+# -*- coding: utf-8 -*-
+import random
+import time as tm
 import main as tp1
 class test:
-    try:
+    start= tm.time()
+    try: 
         
-        print("calcul du perimetre et de la surface du rectangle")
-
-        a=float(input("entrer la longueur : "))
-        b=float(input("entrer la largeur : "))
-
-        c=tp1.rectangle(a,b)
-
-        print("le perimetre est : ",c.perimetre())
-        print("la surface est : ",c.surface())
-        print("calcul du perimetre et de la surface d'un triangle")
-
-        a=float(input("entrer la hauteur : "))
-        b=float(input("entrer le coté 1 : "))
-        c=float(input("entrer le coté 2 : "))
-        d=float(input("entrer le coté 3 : "))
-
-
-        tr=tp1.triangle(a,b,c,d)
-
-        print("le perimetre est : ",tr.perimetre())
-        print("la surface est : ",tr.surface())
-        print("calcul du perimetre et de la surface d'un cercle")
-        a=float(input("entrer le rayon : "))
-        cer=tp1.cercle(a)
-        print("le perimetre est : ",cer.perimetre())
-        print("la surface est : ",cer.surface())
-        
-        print("calcul du perimetre et de la surface du carré ")
-
-        a=float(input("entrer la cote : "))
+         print("calcul du perimetre et de la surface du rectangle")
+         a=float(random.randint(1,50))
+         b=float(random.randint(1,50))
        
-
-        car=tp1.carre(a)
-
-        print("le perimetre est : ",car.perimetre())
-        print("la surface est : ",car.surface())
+         c=tp1.rectangle(a,b)
+         print("le perimetre est : ",c.perimetre())
+         print("la surface est : ",c.surface())
         
-        print("calcul du perimetre et de la surface d'un triangle rectangle")
-
-        a=float(input("entrer la hauteur : "))
-        b=float(input("entrer le coté 1 : "))
-        c=float(input("entrer l'hypotenus' : "))
         
+         print("calcul du perimetre et de la surface d'un triangle")
+         a=float(random.randint(1,50))
+         b=float(random.randint(1,50))
+         c=float(random.randint(1,50))
+         d=float(random.randint(1,50))
+         tr=tp1.triangle(a,b,c,d)
+         print("le perimetre est : ",tr.perimetre())
+         print("la surface est : ",tr.surface())
+        
+        
+         print("calcul du perimetre et de la surface d'un cercle")
+         a=float(random.randint(1,50))
+    
+         cer=tp1.cercle(a)
+         print("le perimetre est : ",cer.perimetre())
+         print("la surface est : ",cer.surface())
 
-
-        triRect=tp1.triangle_rectangle(a,b,c)
-
-        print("le perimetre est : ",triRect.perimetre())
-        print("la surface est : ",triRect.surface())
-
+         print("calcul du perimetre et de la surface du carré ")
+         a=float(random.randint(1,50))
+    
+         car=tp1.carre(a)
+         print("le perimetre est : ",car.perimetre())
+         print("la surface est : ",car.surface())
+        
+        
+         print("calcul du perimetre et de la surface d'un triangle rectangle")
+         a=float(random.randint(1,50))
+         b=float(random.randint(1,50))
+         c=float(random.randint(1,50))
+    
+         triRect=tp1.triangle_rectangle(a,b,c)
+         print("le perimetre est : ",triRect.perimetre())
+         print("la surface est : ",triRect.surface())
     except:
         print("veuiller verifier si toutes vos valeurs sont numeriques ")
+    end=tm.time()
+    temps= end-start
+    
+    print("le temps d'execution est : ",temps)
+    
+    
+    
+    
