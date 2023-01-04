bb543faad3e68ee71ca827ff5104a6b08c29c17c
Nelson-Ngombo <120034661+Nelson-Ngombo@users.noreply.github.com>
2023-01-04 17:56:00 +0100

    implémentation de la class Cercle et Triangle

--- main2.py
+++ main2.py
@@ -10,3 +10,27 @@ class rectangle:
     def perimetre(self):
 
         return (self.haut+self.larg)*2
+import math
+class cercle:
+    def __init__(self,rayon):
+        self.rayon=rayon
+        
+        
+    def surface(self):
+        return math.pi*self.rayon**2
+        
+    def perimetre(self):
+        return 2*math.pi*self.rayon
+
+class triangle:
+    def __init__(self,haut,cote1,cote2,cote3):
+        self.haut=haut
+        self.cote1=cote1
+        self.cote2=cote2
+        self.cote2=cote2
+
+    def surface(self):
+        return self.haut*self.cote1/2
+    
+    def perimetre(self):
+        return (self.cote1+self.cote2+self.cote3)
