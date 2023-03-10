cbc583fe0d64bd0907c5f87c6029e50bc8bfd679
Nelson-Ngombo <120034661+Nelson-Ngombo@users.noreply.github.com>
2023-03-10 20:22:17 +0100

    mise à jour de la batterie de test

--- Batterie_test_data_structure.py
+++ Batterie_test_data_structure.py
@@ -7,6 +7,10 @@ Created on Wed Mar  8 18:57:01 2023
 import LinkedStack as lst
 import LinkedQueue as Lq
 import CircularQueue as Cq
+import ArrayStack as Array
+import ArrayQueue as ArrayQ
+import DynamicArray as Dynm
+
 print("test pour linkstack \n___________________________________\n")
 ls= lst.LinkedStack()
 
@@ -78,6 +82,42 @@ obj.dequeue()
 print("After removing an element from the queue")
 obj.printCQueue()
 
+
+print("test pour ArrayQueue \n___________________________________\n")
+
+ArrQueue=ArrayQ.ArrayQueue()
+for i in range(3):
+    ArrQueue.enqueue(i*3-1)
+    
+    print("La taille du tableau est: ",ArrQueue.__len__())
+
+
+print("test pour DynamicArray \n___________________________________\n")
+
+Dyn=Dynm.DynamicArray()
+print("Le tableau actuel possede {} elements et une capacite  de {}".format(Dyn._n, Dyn._capacity))
+
+for i in range(10):           #creation d'une liste de 10 elements
+    Dyn.append(i)
+print("Le tableau possède {} elements et une capacité de {}".format(len(Dyn), Dyn._capacity))   # En se servant de __getitem__ on cherche la position d'un element dans notre tableau
+
+    
+print("L'element qui occupe la 4ième position est: {}".format(Dyn.__getitem__(5)))
+    
+    # Puisque notre tableau possede 10 élément avec une capacité de 12
+    # Dans ce cas si nous ajoutons 1'élément c'est seulement les nombres d'élément qui changer et non la capacité
+Dyn.append(11)
+print("apres ajout, Le nombre d'élément du tableau est", Dyn._n,"mais la capacité est toujours égale à", Dyn._capacity )
+    
+Dyn.append(12)     # Notre capacité devrait double si on ajoute deux autres elements
+Dyn.append(13)
+print("Le nombre d'élément du tableau est", Dyn._n,"Mais la capacité devient égale à", Dyn._capacity )
+    #Essayons avec un tableau ayant una capacité de 
+Dyn._resize(35)
+print("Le nombre d'élément du tableau est {} et  la capacité devient égale à {}".format(Dyn._n,Dyn._capacity))
+    #On crée un tableau avec une capacité quelconque
+y = Dyn._make_array(10)
+print(y)
 
 
 
